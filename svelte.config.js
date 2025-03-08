903c7d077e5c939dfa91b0dc7e25e9a439e3ecab
Gerrit Wielenga <nova@mail.infernalnova.com>
2025-03-08 01:04:38 -0600

    fix svelte builder

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,14 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			strict: true
+		})
 	}
 };
 
