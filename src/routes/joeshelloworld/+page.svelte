600eb4fa6ddf5c6574949aef78a4f77fa4df55c8
Carguy10 <joseph.stephenson101@gmail.com>
2025-03-18 11:49:20 -0500

    Update
    Hello Christian

--- src/routes/joeshelloworld/+page.svelte
+++ src/routes/joeshelloworld/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
+	import Navbar from '$lib/Navbar.svelte';
 </script>
 
-<main>
-	<h1>Hello WOrld!!!!! :3</h1>
-</main>
+<Navbar page="joeshelloworld" />
+	<h1 class="text-success-500 text-5xl">Hello world!! :3
+		<p>
+			GREEN TEXT
+		</p>
+	</h1>
+	<p>
+		This is the story all about how my life got flipped turned upside-down
+	</p>
