cd5e8abc19bc51a685a3f7dc30f84932aad7374b
Gerrit Wielenga <nova@mail.infernalnova.com>
2025-03-20 00:09:00 -0500

    Added big blue things and some images

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import Navbar from '$lib/Navbar.svelte';
+	import { base } from '$app/paths';
+	import HeroHeader from '$lib/HeroHeader.svelte';
 </script>
 
 <Navbar page="Calendar" />
+<HeroHeader bg_image={base + '/images/circuit_board.jpg'}>
+	{#snippet title()}
+		<p>Default Content</p>
+	{/snippet}
+	{#snippet description()}
+		<p>Lorem Ipsum yada yada yada</p>
+	{/snippet}
+</HeroHeader>
 <main>Things Should GO here</main>
