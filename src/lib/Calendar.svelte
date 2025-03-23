a3d822776396ac1b7b27cc35fb32b2760e69cbe1
Gerrit Wielenga <nova@mail.infernalnova.com>
2025-03-23 15:51:27 -0500

    Sad Coffey

--- src/lib/Calendar.svelte
+++ src/lib/Calendar.svelte
@@ -86,6 +86,7 @@
 						</button>
 						<div class="absolute bottom-0 left-0 w-full p-1">
 							{#each eventsForDay(i) as event}
+
 								<Tooltip
 									triggerClasses="w-full max-w-full rounded-sm text-white overflow-hidden whitespace-nowrap text-ellipsis inline-block"
 									positioning={{ placement: 'top' }}
@@ -125,6 +126,7 @@
 										</div>
 									{/snippet}
 								</Tooltip>
+
 							{/each}
 						</div>
 					{/if}
